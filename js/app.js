6f5dc6f3f7036d23615eedf9d3f663b3512aecd0
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-12-05 17:10:37 -0600

    job dun. I liked this branch. nice and eezee

--- js/app.js
+++ js/app.js
@@ -1,77 +1,52 @@
-/**
- * QUOI FAIRE: 
- * Analyse code (il devrait avoir beaucoup de commentaires)
- * Refactor Code
- * Add delete single card functionality
- * 
- */
-
-const DOM = (function() {
-	const elements = {
-		cardsContainer: document.getElementById("cards-container"),
-		prevBtn: document.getElementById("prev"),
-		nextBtn: document.getElementById("next"),
-		currentEl: document.getElementById("current"),
-		showBtn: document.getElementById("show"),
-		hideBtn: document.getElementById("hide"),
-		questionEl: document.getElementById("question"),
-		answerEl: document.getElementById("answer"),
-		addCardBtn: document.getElementById("add-card"),
-		clearBtn: document.getElementById("clear"),
-		addContainer: document.getElementById("add-container")
-	};
-	return elements;
+const DOM = (function () {
+  const elements = {
+    cardsContainer: document.getElementById("cards-container"),
+    prevBtn: document.getElementById("prev"),
+    nextBtn: document.getElementById("next"),
+    currentEl: document.getElementById("current"),
+    showBtn: document.getElementById("show"),
+    hideBtn: document.getElementById("hide"),
+    questionEl: document.getElementById("question"),
+    answerEl: document.getElementById("answer"),
+    addCardBtn: document.getElementById("add-card"),
+    clearBtn: document.getElementById("clear"),
+    addContainer: document.getElementById("add-container"),
+  };
+  return elements;
 })();
 
-// Traquer la carte actuelle
 let currentActiveCard = 0;
 
-// Garder les DOM cards
 const cardsEl = [];
 
-// Garder les données des cartes
-// const cardsData = [
-// 	{
-// 		question: "What must a variable begin with?",
-// 		answer: "A letter, $ or _"
-// 	},
-// 	{
-// 		question: "What is a variable?",
-// 		answer: "A container for a piece of data"
-// 	},
-// 	{
-// 		question: "Example of a case sensitive variable?",
-// 		answer: "thisIsAVariable"
-// 	}
-// ];
-const cardsData = getCardsData();
+const cardsData = [
+  {
+    question: "What must a variable begin with?",
+    answer: "A letter, $ or _",
+  },
+  {
+    question: "What is a variable?",
+    answer: "A container for a piece of data",
+  },
+  {
+    question: "Example of a case sensitive variable?",
+    answer: "thisIsAVariable",
+  },
+];
 
-/**
- * This function is used to show the number of card out of total number of cards.
- */
 function updateCurrentText() {
-	DOM.currentEl.innerText = `${currentActiveCard + 1}/${cardsEl.length}`;
+  DOM.currentEl.innerText = `${currentActiveCard + 1}/${cardsEl.length}`;
 }
 
-/**
- * This function creates a single card
- * @param {*} dataObj 
- * @param {*} index 
- */
 function createSingleCard(dataObj, index) {
-	// recreating the card first by creating a div
-	const card = document.createElement("div");
-	// ... and giving it the card class
-	card.classList.add("card");
+  const card = document.createElement("div");
+  card.classList.add("card");
 
-	// if the index of the card is 0 (ie, if it is the first card)
-	if (index === 0) {
-		// give it the active class
-		card.classList.add("active");
-	}
+  if (index === 0) {
+    card.classList.add("active");
+  }
 
-	// inject the innerHTML into the just-created card div.
-	card.innerHTML = `
+  card.innerHTML = `
   <div class="inner__card">
       <div class="inner__card--front">
         <p>
@@ -87,111 +62,86 @@ function createSingleCard(dataObj, index) {
   </div>
   `;
 
-	// add a clickListener to the card
-	card.addEventListener("click", () => {
-		// toggle the show-answer class
-		debugger;
-		card.classList.toggle("show-answer");
-		//TODO: put delete single card funcitonality here
-	});
+  card.addEventListener("click", () => {
+    card.classList.toggle("show-answer");
+  });
 
-	// after creating this card, we add it to our cardsElement array
-	cardsEl.push(card);
-	// inject this card to the cardscontainer
-	DOM.cardsContainer.appendChild(card);
+  cardsEl.push(card);
+  DOM.cardsContainer.appendChild(card);
 
-	updateCurrentText();
+  updateCurrentText();
 }
 
-// Create all cards
 function createCards() {
-	// loop through the cards data array; our array of objects
-	// upon each iteration, run the createSingleCard() passing in the object data and the index
-	cardsData.forEach((currentCardData, index) => createSingleCard(currentCardData, index));
+  cardsData.forEach((currentCardData, index) => createSingleCard(currentCardData, index));
 }
 
 createCards();
 
+//NEW:
 DOM.nextBtn.addEventListener("click", () => {
-	cardsEl[currentActiveCard].className = "card left";
-	currentActiveCard = currentActiveCard + 1;
+  // the card that was the current card will be moved to the left
+  cardsEl[currentActiveCard].className = "card left";
+  // the index of the active card becomes the card that is next in the queue. Next becomes current, and current becomes past. Ecclesiastes tings.
+  currentActiveCard = currentActiveCard + 1;
 
-	if (currentActiveCard > cardsEl.length - 1) {
-		currentActiveCard = cardsEl.length - 1;
-	}
+  // if we get to the end of our cards
+  if (currentActiveCard > cardsEl.length - 1) {
+    // the current card will stick to be the last card, no matter how many times we click.
+    //IDEA: why don't we do a cycle? so it goes back to the first card? I'll test it in a later branch
+    currentActiveCard = cardsEl.length - 1;
+  }
 
-	cardsEl[currentActiveCard].className = "card active";
-	console.log(currentActiveCard);
+  // the new current card (look at line 90 if it's still confusing) now gets the classes card and active
+  cardsEl[currentActiveCard].className = "card active";
+  console.log(currentActiveCard);
 
-	updateCurrentText();
+  updateCurrentText();
 });
 
+// Same exact code as the one above, but for clicking previous. Same thing but backwards.
+//TODO: Refactor this into one function and call it twice accordingly
 DOM.prevBtn.addEventListener("click", () => {
-	cardsEl[currentActiveCard].className = "card right";
-	currentActiveCard = currentActiveCard - 1;
+  cardsEl[currentActiveCard].className = "card right";
+  currentActiveCard = currentActiveCard - 1;
 
-	if (currentActiveCard < 0) {
-		currentActiveCard = 0;
-	}
+  //IDEA: just as the one before, if we get to the first, let's try go all the way to the last card, so it's a cycle
+  if (currentActiveCard < 0) {
+    currentActiveCard = 0;
+  }
 
-	cardsEl[currentActiveCard].className = "card active";
-	console.log(currentActiveCard);
+  cardsEl[currentActiveCard].className = "card active";
+  console.log(currentActiveCard);
 
-	updateCurrentText();
+  updateCurrentText();
 });
 
 // show add container
 DOM.showBtn.addEventListener("click", () => {
-	DOM.addContainer.classList.add("show");
+  DOM.addContainer.classList.add("show");
 });
 
 // hide container
 DOM.hideBtn.addEventListener("click", () => {
-	DOM.addContainer.classList.remove("show");
+  DOM.addContainer.classList.remove("show");
 });
 
 // add new card
 DOM.addCardBtn.addEventListener("click", () => {
-	const question = DOM.questionEl.value;
-	const answer = DOM.answerEl.value;
+  const question = DOM.questionEl.value;
+  const answer = DOM.answerEl.value;
 
-	if (question.trim() && answer.trim()) {
-		const newCard = {
-			question: question,
-			answer: answer
-		};
-		createSingleCard(newCard);
-		DOM.questionEl.value = "";
-		DOM.answerEl.value = "";
+  if (question.trim() && answer.trim()) {
+    const newCard = {
+      question: question,
+      answer: answer,
+    };
+    createSingleCard(newCard);
+    DOM.questionEl.value = "";
+    DOM.answerEl.value = "";
 
-		DOM.addContainer.classList.remove("show");
-		cardsData.push(newCard);
-		setCardsData(cardsData);
-	}
-});
-
-// get cards from local storage
-function getCardsData() {
-	const cards = JSON.parse(localStorage.getItem("cards"));
-	return cards === null ? [] : cards;
-}
-
-// add card to local storage
-function setCardsData(cards) {
-	localStorage.setItem("cards", JSON.stringify(cards));
-	// how to reload the page. nice
-	window.location.reload();
-}
-
-// clear cards button
-DOM.clearBtn.addEventListener("click", () => {
-	localStorage.clear();
-	DOM.cardsContainer.innerHTMl = "";
-	window.location.reload();
-});
-
-const deleteCard = document.getElementById("delete-card");
-deleteCard.addEventListener("click", () => {
-	// alert("CLICKED AND WORKED");
-	DOM.addContainer.classList.remove("show-answer");
+    DOM.addContainer.classList.remove("show");
+    cardsData.push(newCard);
+    setCardsData(cardsData);
+  }
 });
--- readme.md
+++ readme.md
@@ -1,12 +1,4 @@
-## Memory Cards
+## Previous & Next Button Functionality
 
-Flash card app for learning. Display, add and remove memory cards with questions and answers
+we can now navigate through the cards, next up is to add new cards and clear all cards.
 
-## Project Specifications
-
-- Create flip cards using CSS
-- Create "Add new card" overlay with form
-- Display question cards and flip for answer
-- View prev and next cards
-- Add new cards to local storage
-- Clear all cards from local storage
