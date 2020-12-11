11b27ea9a7304123d750a4f9df859f3dbcb72fe2
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-12-10 22:25:27 -0600

    we jusss chillin

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+/**
+ * QUOI FAIRE:
+ * Analyse code (il devrait avoir beaucoup de commentaires)
+ * Refactor Code
+ * Add delete single card functionality
+ *
+ */
+
 const DOM = (function () {
   const elements = {
     cardsContainer: document.getElementById("cards-container"),
@@ -15,65 +23,90 @@ const DOM = (function () {
   return elements;
 })();
 
+// Traquer la carte actuelle
 let currentActiveCard = 0;
 
+// Garder les DOM cards
 const cardsEl = [];
 
-let cardsData = [
-  {
-    question: "What must a variable begin with?",
-    answer: "A letter, $ or _",
-  },
-  {
-    question: "What is a variable?",
-    answer: "A container for a piece of data",
-  },
-  {
-    question: "Example of a case sensitive variable?",
-    answer: "thisIsAVariable",
-  },
-];
+// Garder les données des cartes
+// const cardsData = [
+// 	{
+// 		question: "What must a variable begin with?",
+// 		answer: "A letter, $ or _"
+// 	},
+// 	{
+// 		question: "What is a variable?",
+// 		answer: "A container for a piece of data"
+// 	},
+// 	{
+// 		question: "Example of a case sensitive variable?",
+// 		answer: "thisIsAVariable"
+// 	}
+// ];
+const cardsData = getCardsData();
 
+/**
+ * This function is used to show the number of card out of total number of cards.
+ */
 function updateCurrentText() {
   DOM.currentEl.innerText = `${currentActiveCard + 1}/${cardsEl.length}`;
 }
 
+/**
+ * This function creates a single card
+ * @param {*} dataObj
+ * @param {*} index
+ */
 function createSingleCard(dataObj, index) {
+  // recreating the card first by creating a div
   const card = document.createElement("div");
+  // ... and giving it the card class
   card.classList.add("card");
 
+  // if the index of the card is 0 (ie, if it is the first card)
   if (index === 0) {
+    // give it the active class
     card.classList.add("active");
   }
 
+  // inject the innerHTML into the just-created card div.
   card.innerHTML = `
-	<div class="inner__card">
-		<div class="inner__card--front">
-		  <p>
-			${dataObj.question}
-		  </p>
-		  <i id="delete-card" class="fas fa-trash"></i>
-		</div>
-		<div class="inner__card--back">
-		  <p>
-			${dataObj.answer}
-		  </p>
-		</div>
-	</div>
-	`;
+  <div class="inner__card">
+      <div class="inner__card--front">
+        <p>
+          ${dataObj.question}
+        </p>
+        <i id="delete-card" class="fas fa-trash"></i>
+      </div>
+      <div class="inner__card--back">
+        <p>
+          ${dataObj.answer}
+        </p>
+      </div>
+  </div>
+  `;
 
+  // add a clickListener to the card
   card.addEventListener("click", () => {
+    // toggle the show-answer class
+    debugger;
     card.classList.toggle("show-answer");
     //TODO: put delete single card funcitonality here
   });
 
+  // after creating this card, we add it to our cardsElement array
   cardsEl.push(card);
+  // inject this card to the cardscontainer
   DOM.cardsContainer.appendChild(card);
 
   updateCurrentText();
 }
 
+// Create all cards
 function createCards() {
+  // loop through the cards data array; our array of objects
+  // upon each iteration, run the createSingleCard() passing in the object data and the index
   cardsData.forEach((currentCardData, index) => createSingleCard(currentCardData, index));
 }
 
@@ -107,17 +140,17 @@ DOM.prevBtn.addEventListener("click", () => {
   updateCurrentText();
 });
 
-// NEW: show add container - the one with the form
+// show add container
 DOM.showBtn.addEventListener("click", () => {
   DOM.addContainer.classList.add("show");
 });
 
-// NEW: hide container upon clicking the "x" button in the form
+// hide container
 DOM.hideBtn.addEventListener("click", () => {
   DOM.addContainer.classList.remove("show");
 });
 
-// NEW: add new card
+// add new card
 DOM.addCardBtn.addEventListener("click", () => {
   const question = DOM.questionEl.value;
   const answer = DOM.answerEl.value;
@@ -132,23 +165,33 @@ DOM.addCardBtn.addEventListener("click", () => {
     DOM.answerEl.value = "";
 
     DOM.addContainer.classList.remove("show");
-    // cardsData.push(newCard); i like spread better. i'm sorry i'm not sori
-    cardsData = [...cardsData, newCard];
+    cardsData.push(newCard);
+    setCardsData(cardsData);
   }
 });
 
-// FAIL: delete single card functionality
-cardsEl.forEach((currentCard, index) => {
-  let idx = index;
-  currentCard.addEventListener("click", (e) => {
-    if (e.target.id === "delete-card") {
-      currentCard.classList.remove("show-answer");
-      console.log("Clicked the trash bin", idx);
+// get cards from local storage
+function getCardsData() {
+  const cards = JSON.parse(localStorage.getItem("cards"));
+  return cards === null ? [] : cards;
+}
 
-      cardsData = cardsData.filter((currentData, index) => {
-        return index !== idx;
-      });
-      console.log(cardsData);
-    }
-  });
+// add card to local storage
+function setCardsData(cards) {
+  localStorage.setItem("cards", JSON.stringify(cards));
+  // how to reload the page. nice
+  window.location.reload();
+}
+
+// clear cards button
+DOM.clearBtn.addEventListener("click", () => {
+  localStorage.clear();
+  DOM.cardsContainer.innerHTMl = "";
+  window.location.reload();
+});
+
+const deleteCard = document.getElementById("delete-card");
+deleteCard.addEventListener("click", () => {
+  // alert("CLICKED AND WORKED");
+  DOM.addContainer.classList.remove("show-answer");
 });
